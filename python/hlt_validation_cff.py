6e8c96b618e721238cd7652cb89f9c12e65f3445
Jan-Frederik <jschulte@cern.ch>
2018-09-04 19:01:45 +0200

    add correct validation file

--- python/hlt_validation_cff.py
+++ python/hlt_validation_cff.py
@@ -1,22 +1,20 @@
 
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process( "Validation" )
+from Validation.RecoTrack.HLTmultiTrackValidator_cff import *
 
-process.load("Validation.RecoTrack.HLTmultiTrackValidator_cff")
-process.load("setup_dev_CMSSW_10_0_0_GRun_cff")
 doPixel = True
 doTrack02 = True
 
-process.hltTrackValidator.label = []
+hltTrackValidator.label = []
 if doPixel:
-	process.hltTrackValidator.label.extend([
+	hltTrackValidator.label.extend([
     		"hltPixelTracks",
     		"hltPixelTracksFromTriplets",
     		"hltPixelTracksMerged"
 	])
 if doTrack02:
-	process.hltTrackValidator.label.extend([
+	hltTrackValidator.label.extend([
     		"hltIter0PFlowTrackSelectionHighPurity",
     		"hltIter1PFlowTrackSelectionHighPurity",
     		"hltIter1Merged",
@@ -27,59 +25,44 @@ if doTrack02:
   		"hltDoubletRecoveryPFlowTrackSelectionHighPurity",
     		"hltMergedTracks"
 	])
-process.load("SimGeneral.TrackingAnalysis.trackingParticleNumberOfLayersProducer_cfi")
+from SimGeneral.TrackingAnalysis.trackingParticleNumberOfLayersProducer_cfi import *
 #hltTracksValidationTruth = cms.Sequence(hltTPClusterProducer+hltTrackAssociatorByHits+trackingParticleRecoTrackAsssociation+VertexAssociatorByPositionAndTracks+trackingParticleNumberOfLayersProducer)
-process.hltTracksValidationTruth = cms.Sequence(process.hltTPClusterProducer+process.hltTrackAssociatorByHits+process.trackingParticleNumberOfLayersProducer)
+hltTracksValidationTruth = cms.Sequence(hltTPClusterProducer+hltTrackAssociatorByHits+trackingParticleNumberOfLayersProducer)
 
 
-process.hltMultiTrackValidation = cms.Sequence(
-    process.hltTracksValidationTruth
-    + process.hltTrackValidator
+hltMultiTrackValidation = cms.Sequence(
+    hltTracksValidationTruth
+    + hltTrackValidator
 )
 #from Validation.RecoTrack.associators_cff import *
 
-#hltTrackValidator.tip = cms.double(3.5)
-#hltTrackValidator.lip = cms.double(30)
-#hltTrackValidator.signalOnlyTP = cms.bool(True)
 
+from SimTracker.VertexAssociation.VertexAssociatorByPositionAndTracks_cfi import *
+hltVertexAssociatorByPositionAndTracks = VertexAssociatorByPositionAndTracks.clone()
+hltVertexAssociatorByPositionAndTracks.trackAssociation = "tpToHLTpixelTrackAssociation"
 
-process.load("SimTracker.VertexAssociation.VertexAssociatorByPositionAndTracks_cfi")
-process.hltVertexAssociatorByPositionAndTracks = process.VertexAssociatorByPositionAndTracks.clone()
-process.hltVertexAssociatorByPositionAndTracks.trackAssociation = "tpToHLTpixelTrackAssociation"
-
-process.load("Validation.RecoVertex.HLTmultiPVvalidator_cff")
-process.hltMultiPVanalysis.verbose = False
+from Validation.RecoVertex.HLTmultiPVvalidator_cff import *
+hltMultiPVanalysis.verbose = False
 #hltMultiPVanalysis.trackAssociatorMap = "tpToHLTpixelTrackAssociation"
 #hltMultiPVanalysis.vertexAssociator   = "vertexAssociatorByPositionAndTracks4pixelTracks"
 #tpToHLTpixelTrackAssociation.ignoremissingtrackcollection = False
-process.hltPixelPVanalysis.trackAssociatorMap = "tpToHLTpixelTrackAssociation"
-process.hltPixelPVanalysis.vertexAssociator = "vertexAssociatorByPositionAndTracks4pixelTracks" 
+hltPixelPVanalysis.trackAssociatorMap = "tpToHLTpixelTrackAssociation"
+hltPixelPVanalysis.vertexAssociator = "vertexAssociatorByPositionAndTracks4pixelTracks" 
 
-process.tpToHLTpixelTrackAssociation.label_tr = "hltPixelTracksMerged"
+tpToHLTpixelTrackAssociation.label_tr = "hltPixelTracksMerged"
 
-process.validation = cms.EndPath(
-    process.hltMultiTrackValidation
-    + process.hltTrackAssociatorByHits
-    + process.tpToHLTpixelTrackAssociation
-    + process.hltVertexAssociatorByPositionAndTracks
+validation = cms.EndPath(
+    hltMultiTrackValidation
+    + hltTrackAssociatorByHits
+    + tpToHLTpixelTrackAssociation
+    + hltVertexAssociatorByPositionAndTracks
 #    + hltMultiPVanalysis
-    + process.hltMultiPVValidation
-)
-process.source = cms.Source( "PoolSource",
-    fileNames = cms.untracked.vstring(
-        'file:HLTResults.root',
-    ),
-    inputCommands = cms.untracked.vstring(
-        'keep *'
-    )
+    + hltMultiPVValidation
 )
 
-process.load( "DQMServices.Core.DQMStore_cfi" )
-process.DQMStore.enableMultiThread = True
 
-
-process.dqmOutput = cms.OutputModule("DQMRootOutputModule",
+dqmOutput = cms.OutputModule("DQMRootOutputModule",
     fileName = cms.untracked.string("DQMIO.root")
 )
-process.DQMOutput = cms.EndPath( process.dqmOutput )
+DQMOutput = cms.EndPath( dqmOutput )
 
